28b5160b9d30d3f46907932d599e87d70987c2a6
Arnold Noronha <arnold@tdrhq.com>
2019-10-29 22:34:12 -0700

    wrap at one layer above

--- hunchentoot-multi-acceptor.lisp
+++ hunchentoot-multi-acceptor.lisp
@@ -2,11 +2,19 @@
 
 (in-package #:hunchentoot-multi-acceptor)
 
+(defclass multi-request (request)
+  ())
+
 (defclass multi-acceptor (acceptor)
   ((sub-acceptors :initform nil :accessor sub-acceptors)
    (listen-fd :initarg :listen-fd
               :initform nil
               :accessor listen-fd)))
+
+(defmethod  make-instance ((class (eql 'multi-acceptor)) &rest args)
+  (let ((ret (call-next-method)))
+    (setf (acceptor-request-class ret) 'multi-request)
+    ret))
 
 (defparameter *default-acceptor*
   (make-instance 'hunchentoot:easy-acceptor
@@ -16,18 +24,49 @@
 ;; (define-easy-handler (s-default-message :uri "/foo" :acceptor-names '(default-acceptor)) ()
 ;;   (format nil "error, hostname not set up for: ~a" (host *request*)))
 
+(defun copy-request (request acceptor)
+  (let ((*acceptor* acceptor))
+    (make-instance (acceptor-request-class acceptor)
+                   :acceptor acceptor
+                   :local-addr (local-addr request)
+                   :local-port (local-port request)
+                   :remote-addr (remote-addr request)
+                   :remote-port (remote-port request)
+                   :headers-in (headers-in request)
+                   :content-stream (hunchentoot::content-stream request)
+                   :method (request-method request)
+                   :uri (request-uri request)
+                   :server-protocol (server-protocol request))))
 
-(defmethod acceptor-dispatch-request ((acceptor multi-acceptor) request)
-  (let ((host (car (str:split ":" (host request)))))
-    (loop for sub in (sub-acceptors acceptor)
-       if (equal (car sub) host)
-       do (return-from acceptor-dispatch-request
-            (let ((*acceptor* (cdr sub)))
-              (acceptor-dispatch-request (cdr sub) request))))
+#+nil (copy-request
+ (let ((*acceptor* (make-instance 'acceptor))
+       (*reply* (make-instance 'reply)))
+   (make-instance 'request
+                  :local-addr "343"
+                  :local-port 20
+                  :method nil
+                  :uri nil
+                  :server-protocol nil
+                  :remote-addr "dfdf"
+                  :remote-port 30
+                  :headers-in nil
+                  :content-stream nil)) (make-instance 'acceptor))
 
-    (format *error-output* "oops, nothing available for ~a~%" host)
-    ;; for whatever reason this doesn't work at this point. :(
-    (acceptor-dispatch-request *default-acceptor* request)))
+(defmethod process-request ((request multi-request))
+  (let ((acceptor (request-acceptor request)))
+   (let ((host (car (str:split ":" (host request)))))
+     (loop for sub in (sub-acceptors acceptor)
+        if (equal (car sub) host)
+        do (return-from process-request
+             (let* ((*acceptor* (cdr sub))
+                    (*request* (copy-request request *acceptor*)))
+               (format t "looking at ~a~%" *acceptor*)
+               (call-next-method))))
+
+     (format t "oops, nothing available for ~a~%" host)
+     ;; for whatever reason this doesn't work at this point. :(
+     (let ((*acceptor* *default-acceptor*))
+       (call-next-method)))))
 
 (defun listen-on-fd (fd &key element-type)
   (let ((sock (make-instance 'sb-bsd-sockets:inet-socket
